aeeb417d79a480665b04a1e8bf0c54c3b3980d91
Scott McKendry <39483124+scottmckendry@users.noreply.github.com>
2024-05-20 14:07:44 +1200

    refactoring

--- setup.sh
+++ setup.sh
@@ -1,5 +1,47 @@
 #!/bin/bash
 
+# Symlinks: Target -> Destination
+declare -A mappings=(
+	["$HOME/git/dots/.bash_profile"]=$HOME/.bash_profile
+	["$HOME/git/dots/.bashrc"]=$HOME/.bashrc
+	["$HOME/git/dots/.gitconfig"]=$HOME/.gitconfig
+	["$HOME/git/dots/.tmux.conf"]=$HOME/.tmux.conf
+	["$HOME/git/dots/alacritty"]=$HOME/.config/alacritty
+	["$HOME/git/dots/lazygit/"]=$HOME/.config/lazygit
+	["$HOME/git/dots/nvim"]=$HOME/.config/nvim
+)
+
+# Dependencies
+deps=(
+	"alacritty"
+	"azure-cli-bin"
+	"bash-completion"
+	"bat"
+	"bat"
+	"bicep-bin"
+	"eza"
+	"fastfetch"
+	"fuse2"
+	"fuse2fs"
+	"fuse3"
+	"fzf"
+	"github-cli"
+	"go"
+	"lazygit"
+	"neovim-git"
+	"nodejs"
+	"npm"
+	"powershell-bin"
+	"python"
+	"ripgrep"
+	"screen"
+	"starship"
+	"ttf-jetbrains-mono-nerd"
+	"unzip"
+	"wget"
+	"zoxide"
+)
+
 # Install yay if not already installed
 if ! command -v yay &>/dev/null; then
 	echo "yay could not be found, installing..."
@@ -19,15 +61,6 @@ yay -Syu --noconfirm
 # Change to the directory of this script
 cd "$(dirname "$0")"
 
-declare -A mappings=(
-	["$HOME/git/dots/nvim"]=$HOME/.config/nvim
-	["$HOME/git/dots/.gitconfig"]=$HOME/.gitconfig
-	["$HOME/git/dots/alacritty"]=$HOME/.config/alacritty
-	["$HOME/git/dots/.bashrc"]=$HOME/.bashrc
-	["$HOME/git/dots/.bash_profile"]=$HOME/.bash_profile
-	["$HOME/git/dots/lazygit/"]=$HOME/.config/lazygit
-)
-
 echo "Removing existing files/directories..."
 for key in "${!mappings[@]}"; do
 	rm -rf ${mappings[$key]}
@@ -39,11 +72,10 @@ for key in "${!mappings[@]}"; do
 done
 
 echo "Installing Dependencies..."
-yay -S --needed --noconfirm alacritty python fzf ripgrep bat bash-completion screen go lazygit eza zoxide starship ttf-jetbrains-mono-nerd nodejs npm powershell-bin azure-cli-bin bicep-bin github-cli unzip fastfetch wget bat fuse3 fuse2 fuse2fs
-
-echo "Install Neovim Nightly"
-wget https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage -O /tmp/nvim
-chmod +x /tmp/nvim
-sudo mv /tmp/nvim /usr/local/bin/nvim
+depString=""
+for dep in "${deps[@]}"; do
+	depString="$depString $dep"
+done
+yay -S --noconfirm --needed $depString
 
 echo "Done!"
