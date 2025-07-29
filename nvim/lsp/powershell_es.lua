97d4ba33354cbae114c7bf2ab7a3929bea9bf2e8
Scott McKendry <39483124+scottmckendry@users.noreply.github.com>
2025-07-29 03:00:25 +0000

    screenshare window rule [skip ci] Automated sync from scottmckendry/nix

--- nvim/lsp/powershell_es.lua
+++ nvim/lsp/powershell_es.lua
@@ -2,7 +2,6 @@ return {
     cmd = function(dispatchers)
         local temp_path = vim.fn.stdpath("cache")
         local bundle_path = vim.fn.stdpath("data") .. "/mason/packages/powershell-editor-services"
-        vim.notify("Loading PowerShell Editor Services from " .. bundle_path, vim.log.levels.INFO)
         local shell = "pwsh"
         local command_fmt =
             [[& '%s/PowerShellEditorServices/Start-EditorServices.ps1' -BundledModulesPath '%s' -LogPath '%s/powershell_es.log' -SessionDetailsPath '%s/powershell_es.session.json' -FeatureFlags @() -AdditionalModules @() -HostName nvim -HostProfileId 0 -HostVersion 1.0.0 -Stdio -LogLevel Normal]]
